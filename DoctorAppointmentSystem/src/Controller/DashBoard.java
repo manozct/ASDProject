52d4ad99836829be8b0dfdd1ef0e3c1c31a28140
ipKandel <eesscan@gmail.com>
2017-06-18 20:24:15 -0500

    Merge branch 'master' of https://github.com/manozct/ASDProject
    # Conflicts:
    #	src/com/asd/framework/Appointment/WaitingAppointment.java
    #	src/com/asd/framework/Calendar/Calendar.java
    #	src/com/asd/framework/Calendar/CalendarTest.java
    #	src/com/asd/framework/DatabaseConnection/Db/DbConnection.java

--- DoctorAppointmentSystem/src/Controller/DashBoard.java
+++ DoctorAppointmentSystem/src/Controller/DashBoard.java
@@ -1,18 +1,23 @@
 package Controller;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 
 import javax.swing.*;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by manozct on 6/15/2017.
@@ -24,6 +29,7 @@ public class DashBoard extends Application {
     @FXML Menu customerListId;
     @FXML Menu doctorProfileId;
     @FXML MenuBar MainMenu;
+    @FXML MenuItem myDashBoard;
 
     private HashMap<String,List<Menu>>hMapControlByRole=new HashMap<>();
 
@@ -44,6 +50,34 @@ public class DashBoard extends Application {
 
     }
 
+    @FXML
+    void viewDashBoard(ActionEvent event)  {
+        System.out.println("test");
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../View/Appointment.fxml"));
+        try {
+            Stage stage=new Stage();
+            stage.setScene(new Scene(loader.load()));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+    public void designMainMenu(){
+        JPopupMenu popupMenu = new JPopupMenu();
+
+        JMenu deviceMenu = new JMenu("Add Device");
+
+        popupMenu.add(deviceMenu);
+        popupMenu.add(new JMenuItem("Delete Device"));
+        popupMenu.add(new JMenuItem("Fire"));
+        popupMenu.add(new JMenuItem("Fault"));
+        popupMenu.add(new JMenuItem("Supress"));
+    }
 
 
 
